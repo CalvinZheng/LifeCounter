c8f0c1b4078ce81261886d16b64a8ee95c18eb26
Calvin Zheng <calvin.zheng@corel.com>
2019-08-19 15:31:54 -0400

    Initial Commit

---
+++ LifeCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCounter
+//
+//  Created by Haomin Zheng on 2019-08-19.
+//  Copyright © 2019 Haomin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
