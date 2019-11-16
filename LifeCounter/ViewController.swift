a05e0f9e13c4de0d865a80116de7b6a1dc9620bb
Calvin Zheng <calvinz.mcgill@gmail.com>
2019-11-16 16:37:47 +1000

    Add 2nd player life count

--- LifeCounter/ViewController.swift
+++ LifeCounter/ViewController.swift
@@ -11,21 +11,36 @@ import UIKit
 class ViewController: UIViewController {
 	
 	var currentLifeTotal = 20
+    var currentTopLifeTotal = 20
 	
-	
+    @IBOutlet weak var topLifeLabel: UILabel!
+    
 	@IBOutlet weak var lifeLabel: UILabel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 		
+        topLifeLabel.transform = CGAffineTransform(rotationAngle: CGFloat(Float.pi))
+        topLifeLabel.text = "\(currentTopLifeTotal)"
 		lifeLabel.text = "\(currentLifeTotal)"
 	}
 
+    @IBAction func topMinus(_ sender: Any) {
+        currentTopLifeTotal -= 1
+        topLifeLabel.text = "\(currentTopLifeTotal)"
+    }
+    
+    @IBAction func topPlus(_ sender: Any) {
+        currentTopLifeTotal += 1
+        topLifeLabel.text = "\(currentTopLifeTotal)"
+    }
+    
 	@IBAction func minus(_ sender: Any) {
 		currentLifeTotal -= 1
 		lifeLabel.text = "\(currentLifeTotal)"
 	}
+    
 	@IBAction func plus(_ sender: Any) {
 		currentLifeTotal += 1
 		lifeLabel.text = "\(currentLifeTotal)"
